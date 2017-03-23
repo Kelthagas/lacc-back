9a35eaceb12acb48cf4cc546d82e7c4ee55b8ef0
Andrew <andrew@lacomputercompany.com>
2017-03-23 11:13:50 -0700

    more database

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,8 @@
 class SessionsController < ApplicationController
 
+  def new
+  end
+
   def create
     user = User.find_by(user_id: params[:session][:user_id])
     if user && user.authenticate(params[:session][:password])
@@ -10,5 +13,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to(root_path)
   end
 end
